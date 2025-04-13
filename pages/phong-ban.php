144dd7764871e472b7edec2cb6dd71582b8bfc7d
SachHoang3393 <hoangsach2003@gmail.com>
2025-04-13 17:52:45 +0700

    SCRUM-68 Xử lý backend #comment Đã hoàn thành phần backend #time 3h #in-progress

--- pages/phong-ban.php
+++ pages/phong-ban.php
@@ -1,80 +1,80 @@
 <?php 
 
-// create session
-// session_start();
+//create session
+session_start();
 
-// if(isset($_SESSION['username']) && isset($_SESSION['level']))
-// {
-//   // include file
-//   include('../layouts/header.php');
-//   include('../layouts/topbar.php');
-//   include('../layouts/sidebar.php');
+if(isset($_SESSION['username']) && isset($_SESSION['level']))
+{
+  // include file
+  include('../layouts/header.php');
+  include('../layouts/topbar.php');
+  include('../layouts/sidebar.php');
 
-//   if(isset($_POST['edit']))
-//   {
-//     $id = $_POST['idRoom'];
-//     echo "<script>location.href='sua-phong-ban.php?p=staff&a=room&id=".$id."'</script>";
-//   }
+  if(isset($_POST['edit']))
+  {
+    $id = $_POST['idRoom'];
+    echo "<script>location.href='sua-phong-ban.php?p=staff&a=room&id=".$id."'</script>";
+  }
 
-//   // show data
-//   $showData = "SELECT id, ma_phong_ban, ten_phong_ban, ghi_chu, nguoi_tao, ngay_tao, nguoi_sua, ngay_sua FROM phong_ban ORDER BY ngay_tao DESC";
-//   $result = mysqli_query($conn, $showData);
-//   $arrShow = array();
-//   while ($row = mysqli_fetch_array($result)) {
-//     $arrShow[] = $row;
-//   }
+  // show data
+  $showData = "SELECT id, ma_phong_ban, ten_phong_ban, ghi_chu, nguoi_tao, ngay_tao, nguoi_sua, ngay_sua FROM phong_ban ORDER BY ngay_tao DESC";
+  $result = mysqli_query($conn, $showData);
+  $arrShow = array();
+  while ($row = mysqli_fetch_array($result)) {
+    $arrShow[] = $row;
+  }
 
-//   // create code room
-//   $roomCode = "MBP" . time();
+  // create code room
+  $roomCode = "MBP" . time();
 
-//   // delete record
-//   if(isset($_POST['save']))
-//   {
-//     // create array error
-//     $error = array();
-//     $success = array();
-//     $showMess = false;
+  // delete record
+  if(isset($_POST['save']))
+  {
+    // create array error
+    $error = array();
+    $success = array();
+    $showMess = false;
 
-//     // get id in form
-//     $roomName = $_POST['roomName'];
-//     $description = $_POST['description'];
-//     $personCreate = $row_acc['ho'] . $row_acc['ten'];
-//     $dateCreate = date("Y-m-d H:i:s");
-//     $personEdit = $row_acc['ho'] . $row_acc['ten'];
-//     $dateEdit = date("Y-m-d H:i:s");
+    // get id in form
+    $roomName = $_POST['roomName'];
+    $description = $_POST['description'];
+    $personCreate = $row_acc['ho'] . $row_acc['ten'];
+    $dateCreate = date("Y-m-d H:i:s");
+    $personEdit = $row_acc['ho'] . $row_acc['ten'];
+    $dateEdit = date("Y-m-d H:i:s");
 
-//     // validate
-//     if(empty($roomName))
-//       $error['roomName'] = 'Vui lòng nhập <b> tên phòng ban </b>';
+    // validate
+    if(empty($roomName))
+      $error['roomName'] = 'Vui lòng nhập <b> tên phòng ban </b>';
 
-//     if(!$error)
-//     {
-//       $showMess = true;
-//       $insert = "INSERT INTO phong_ban(ma_phong_ban, ten_phong_ban, ghi_chu, nguoi_tao, ngay_tao, nguoi_sua, ngay_sua) VALUES('$roomCode','$roomName', '$description', '$personCreate', '$dateCreate', '$personEdit', '$dateEdit')";
-//       mysqli_query($conn, $insert);
-//       $success['success'] = 'Tạo phòng ban thành công.';
-//       echo '<script>setTimeout("window.location=\'phong-ban.php?p=staff&a=room\'",1000);</script>';
-//     }
+    if(!$error)
+    {
+      $showMess = true;
+      $insert = "INSERT INTO phong_ban(ma_phong_ban, ten_phong_ban, ghi_chu, nguoi_tao, ngay_tao, nguoi_sua, ngay_sua) VALUES('$roomCode','$roomName', '$description', '$personCreate', '$dateCreate', '$personEdit', '$dateEdit')";
+      mysqli_query($conn, $insert);
+      $success['success'] = 'Tạo phòng ban thành công.';
+      echo '<script>setTimeout("window.location=\'phong-ban.php?p=staff&a=room\'",1000);</script>';
+    }
 
-//   }
+  }
 
-//   // delete record
-//   if(isset($_POST['delete']))
-//   {
-//     $showMess = true;
+  // delete record
+  if(isset($_POST['delete']))
+  {
+    $showMess = true;
 
-//     $id = $_POST['idRoom'];
-//     $delete = "DELETE FROM phong_ban WHERE id = $id";
-//     mysqli_query($conn, $delete);
-//     $success['success'] = 'Xóa phòng ban thành công.';
-//     echo '<script>setTimeout("window.location=\'phong-ban.php?p=staff&a=room\'",1000);</script>';
-//   }
+    $id = $_POST['idRoom'];
+    $delete = "DELETE FROM phong_ban WHERE id = $id";
+    mysqli_query($conn, $delete);
+    $success['success'] = 'Xóa phòng ban thành công.';
+    echo '<script>setTimeout("window.location=\'phong-ban.php?p=staff&a=room\'",1000);</script>';
+  }
 
 ?>
 
 
   <!-- Modal -->
-  <!-- <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+   <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <form method="POST">
@@ -95,7 +95,7 @@
         </form>
       </div>
     </div>
-  </div> -->
+  </div> 
 
   <!-- Content Wrapper. Contains page content -->
   <!-- <div class="content-wrapper">
@@ -294,12 +294,12 @@
 
 <?php
   // include
-//   include('../layouts/footer.php');
-// }
-// else
-// {
-//   // go to pages login
-//   header('Location: dang-nhap.php');
-// }
+  include('../layouts/footer.php');
+}
+else
+{
+  // go to pages login
+  header('Location: dang-nhap.php');
+}
 
 ?>
