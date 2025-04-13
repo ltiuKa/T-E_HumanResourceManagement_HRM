a749dae6f85b535efbb3fbe3e9bfcd5ce4fba3df
Thang8047 <anhbao88thang@gmail.com>
2025-04-14 00:06:48 +0700

    SCRUM-142 Giao dien loại khen thưởng # Done

--- pages/sua-loai-khen-thuong.php
+++ pages/sua-loai-khen-thuong.php
@@ -1,62 +1,57 @@
-<!-- <?php 
+<?php
 
 // create session
-// session_start();
+session_start();
 
-// if(isset($_SESSION['username']) && isset($_SESSION['level']))
-// {
-//   // include file
-//   include('../layouts/header.php');
-//   include('../layouts/topbar.php');
-//   include('../layouts/sidebar.php');
+if (isset($_SESSION['username']) && isset($_SESSION['level'])) {
+  // include file
+  include('../layouts/header.php');
+  include('../layouts/topbar.php');
+  include('../layouts/sidebar.php');
 
-//   // show data
-//   if(isset($_GET['id']))
-//   {
-//     $id = $_GET['id'];
-//     $showData = "SELECT id, ma_loai, ten_loai, ghi_chu, nguoi_sua, ngay_sua FROM loai_khen_thuong_ky_luat WHERE id = $id";
-//     $result = mysqli_query($conn, $showData);
-//     $row = mysqli_fetch_array($result);
-//     $nguoiSua = $row_acc['ho'] . " " . $row_acc['ten'];
-//   }
+  // show data
+  if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $showData = "SELECT id, ma_loai, ten_loai, ghi_chu, nguoi_sua, ngay_sua FROM loai_khen_thuong_ky_luat WHERE id = $id";
+    $result = mysqli_query($conn, $showData);
+    $row = mysqli_fetch_array($result);
+    $nguoiSua = $row_acc['ho'] . " " . $row_acc['ten'];
+  }
 
-//   // save record
-//   if(isset($_POST['save']))
-//   {
-//     // create array error
-//     $error = array();
-//     $success = array();
-//     $showMess = false;
+  // save record
+  if (isset($_POST['save'])) {
+    // create array error
+    $error = array();
+    $success = array();
+    $showMess = false;
 
-//     // get id in form
-//     $tenLoai = $_POST['tenLoai'];
-//     $moTa = $_POST['moTa'];
-//     $ngaySua = date("Y-m-d H:i:s");
+    // get id in form
+    $tenLoai = $_POST['tenLoai'];
+    $moTa = $_POST['moTa'];
+    $ngaySua = date("Y-m-d H:i:s");
 
-//     // validate
-//     if(empty($tenLoai))
-//       $error['tenLoai'] = 'Vui lòng nhập <b> tên loại </b>';
+    // validate
+    if (empty($tenLoai))
+      $error['tenLoai'] = 'Vui lòng nhập <b> tên loại </b>';
 
-//     if(!$error)
-//     {
-//       $showMess = true;
-//       $update = " UPDATE loai_khen_thuong_ky_luat SET
-//                   ten_loai = '$tenLoai',
-//                   ghi_chu = '$moTa',
-//                   nguoi_sua = '$nguoiSua',
-//                   ngay_sua = '$ngaySua'
-//                   WHERE id = $id";
-//       mysqli_query($conn, $update);
-//       $success['success'] = 'Lưu lại thành công.';
-//       echo '<script>setTimeout("window.location=\'sua-loai-khen-thuong.php?p=bonus-discipline&a=bonus&id='.$id.'\'",1000);</script>';
-//     }
-
-//   }
+    if (!$error) {
+      $showMess = true;
+      $update = " UPDATE loai_khen_thuong_ky_luat SET
+                  ten_loai = '$tenLoai',
+                  ghi_chu = '$moTa',
+                  nguoi_sua = '$nguoiSua',
+                  ngay_sua = '$ngaySua'
+                  WHERE id = $id";
+      mysqli_query($conn, $update);
+      $success['success'] = 'Lưu lại thành công.';
+      echo '<script>setTimeout("window.location=\'sua-loai-khen-thuong.php?p=bonus-discipline&a=bonus&id=' . $id . '\'",1000);</script>';
+    }
+  }
 
 ?>
 
   <!-- Content Wrapper. Contains page content -->
-  <!-- <div class="content-wrapper">
+  <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <h1>
@@ -83,38 +78,32 @@
             </div>
             <!-- /.box-header -->
             <div class="box-body">
-              <?php 
-                // show error
-                if(isset($error)) 
-                {
-                  if($showMess == false)
-                  {
-                    echo "<div class='alert alert-danger alert-dismissible'>";
-                    echo "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>";
-                    echo "<h4><i class='icon fa fa-ban'></i> Lỗi!</h4>";
-                    foreach ($error as $err) 
-                    {
-                      echo $err . "<br/>";
-                    }
-                    echo "</div>";
+              <?php
+              // show error
+              if (isset($error)) {
+                if ($showMess == false) {
+                  echo "<div class='alert alert-danger alert-dismissible'>";
+                  echo "<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>";
+                  echo "<h4><i class='icon fa fa-ban'></i> Lỗi!</h4>";
+                  foreach ($error as $err) {
+                    echo $err . "<br/>";
                   }
+                  echo "</div>";
                 }
+              }
               ?>
-              <?php 
-                // show success
-                if(isset($success)) 
-                {
-                  if($showMess == true)
-                  {
-                    echo "<div class='alert alert-success alert-dismissible'>";
-                    echo "<h4><i class='icon fa fa-check'></i> Thành công!</h4>";
-                    foreach ($success as $suc) 
-                    {
-                      echo $suc . "<br/>";
-                    }
-                    echo "</div>";
+              <?php
+              // show success
+              if (isset($success)) {
+                if ($showMess == true) {
+                  echo "<div class='alert alert-success alert-dismissible'>";
+                  echo "<h4><i class='icon fa fa-check'></i> Thành công!</h4>";
+                  foreach ($success as $suc) {
+                    echo $suc . "<br/>";
                   }
+                  echo "</div>";
                 }
+              }
               ?>
               <form action="" method="POST">
                 <div class="row">
@@ -157,16 +146,14 @@
       <!-- /.row -->
     </section>
     <!-- /.content -->
-  </div> -->
+  </div>
 
 <?php
   // include
-//   include('../layouts/footer.php');
-// }
-// else
-// {
-//   // go to pages login
-//   header('Location: dang-nhap.php');
-// }
+  include('../layouts/footer.php');
+} else {
+  // go to pages login
+  header('Location: dang-nhap.php');
+}
 
-?> -->
+?>
